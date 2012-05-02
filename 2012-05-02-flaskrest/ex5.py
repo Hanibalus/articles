9b3560497f40f435446aad2b9414a3abac9f64e8
Luis Rei <me@luisrei.com>
2012-05-02 13:47:26 +0100

    added example code for flask rest article
    fix redundant directory.

---
+++ 2012-05-02-flaskrest/ex5.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for
+from flask import request
+from flask import json
+app = Flask(__name__)
+
+from flask import Response
+
+@app.route('/hello', methods = ['GET'])
+def api_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+    return resp
+    
+
+if __name__ == '__main__':
+    app.run()
